291d014c327a56024180f0e7f33e279e81747067
giangminhhoang <hoang@inspireui.com>
2020-03-06 18:31:51 +0700

    Api Coffe Version One

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cros = require('cors')
+require('dotenv/config');
+
+app.use(cors())
+app.use(bodyParser.json());
+// //Middlewares
+// app.use('/post',()=>{
+//     console.log('This is middewares running')
+// })
+//Import Router 
+const postsRoute = require('./routes/posts');
+app.use('/posts',postsRoute);
+
+
+//ROUTES
+app.get('/',(req,res)=>{
+    res.send('We are on home'); 
+})
+app.get('/posts',(req,res)=>{
+    res.send('We are on post'); 
+})
+//Connect TO DB
+mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser:true , useCreateIndex:true},()=>console.log('conected to DB'));
+
+//How to we start lsitening to the server
+app.listen(3000);
